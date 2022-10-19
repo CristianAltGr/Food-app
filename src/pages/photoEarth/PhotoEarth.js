a28fbf577e51378464e544e480f45b37917c3ba0
Cristian <crifergon1@gmail.com>
2022-10-19 04:11:39 +0200

    Photos earth logic finished!

--- src/pages/photoEarth/PhotoEarth.js
+++ src/pages/photoEarth/PhotoEarth.js
@@ -5,30 +5,26 @@ import EarthStyle from './PhotoEarthStyle';
 const PhotoEarth = () => {
 
     const today = new Date();
-    const [day, setDay] = useState(today.getDate() - 5);
-    const [month, setMonth] = useState(today.getMonth() + 1);
-    const [year, setYear] = useState(today.getFullYear());
+    const day = today.getDate() - 5;
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
     const [images, setImages] = useState([]);
     const [date, setDate] = useState(`${year}-${month}-${day}`);
-
-    const earthPhotos = async () => {
-
-        const list = await getEarthPhoto(date);
-        setImages(list);
-    }
 
     const dateChange = (event) => {
         const value = event.target.value;
 
         if (value.length >= 10) {
             setDate(value);
-            setDay(value.slice(8, 10));
-            setMonth(value.slice(5, 7));
-            setYear(value.slice(0, 4));
         }
     }
 
     useEffect(() => {
+        const earthPhotos = async () => {
+
+            const list = await getEarthPhoto(date);
+            setImages(list);
+        }
         earthPhotos();
     }, [date]);
 
@@ -40,13 +36,13 @@ const PhotoEarth = () => {
             <h2>Photos of the Earth</h2>
             <section>
                 <p>These photos are made on {date}</p>
-                <p>You can search photos of any day: "Note: Nasa reports photos since a week"</p>
+                <p>You can search photos of any day: "Note: NASA sends photos until last week"</p>
                 <input type="date" name="date" id="date" onChange={dateChange} />
             </section>
 
             {images.map(image => {
 
-                const url = `https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/png/epic_1b_${image.identifier}.png?api_key=HTZi2cPx9Q4rCTbCy3SrBnQY4cIHe7eWeJdT8TZw`;
+                const url = `https://api.nasa.gov/EPIC/archive/natural/${image.date.slice(0, 4)}/${image.date.slice(5, 7)}/${image.date.slice(8, 10)}/png/epic_1b_${image.identifier}.png?api_key=HTZi2cPx9Q4rCTbCy3SrBnQY4cIHe7eWeJdT8TZw`;
 
                 return (
                     <div key={image.identifier}>
@@ -57,7 +53,6 @@ const PhotoEarth = () => {
             })}
 
         </EarthStyle>
-
     );
 }
 
