2504bcea3a91c1c293889e72614fcb28d03ac749
Cristian <crifergon1@gmail.com>
2022-10-20 02:42:12 +0200

    Call item asteroid solved

--- src/services/AsteroidData.js
+++ src/services/AsteroidData.js
@@ -2,11 +2,12 @@ import axios from "axios";
 import APIKey from "./APIKey";
 //`https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${APIKey}`
 //https://api.nasa.gov/neo/rest/v1/neo/2312942?api_key=HTZi2cPx9Q4rCTbCy3SrBnQY4cIHe7eWeJdT8TZw
+//https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=DEMO_KEY
 const GetAsteriodData = async (id) => {
 
     try {
 
-        const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=DEMO_KEY`)
+        const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/neo/${id}?api_key=${APIKey}`)
             .then((res) => res.data);
         console.log(response);
         return response;
